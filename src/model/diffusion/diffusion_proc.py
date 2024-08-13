12c68b34e6e4dacc645f471ac32deda11945c412
Anirbit Ghosh <2439281g@student.gla.ac.uk>
2024-08-13 18:48:30 +0100

    Evaluatioon and Inference

--- src/model/diffusion/diffusion_proc.py
+++ src/model/diffusion/diffusion_proc.py
@@ -5,14 +5,38 @@ from tqdm import tqdm
 import math
 
 # Helper functions for beta schedules
-def linear_noise_schedule(timesteps):
-    return torch.linspace(0.0001, 0.02, timesteps)
+# def linear_noise_schedule(timesteps):
+#     return torch.linspace(0.0001, 0.02, timesteps)
 
-def cosine_noise_schedule(timesteps):
-    return torch.tensor([math.cos(i / timesteps * math.pi / 2) for i in range(timesteps)])
+# def cosine_noise_schedule(timesteps):
+#     return torch.tensor([math.cos(i / timesteps * math.pi / 2) for i in range(timesteps)])
+
+# def sigmoid_noise_schedule(timesteps):
+#     return torch.tensor([1 / (1 + math.exp(-10 * (i / timesteps - 0.5))) for i in range(timesteps)])
+
+# beta schedule
+def linear_noise_schedule(timesteps):
+    scale = 1000 / timesteps
+    beta_start = scale * 0.0001
+    beta_end = scale * 0.02
+    return torch.linspace(beta_start, beta_end, timesteps, dtype=torch.float64)
 
 def sigmoid_noise_schedule(timesteps):
-    return torch.tensor([1 / (1 + math.exp(-10 * (i / timesteps - 0.5))) for i in range(timesteps)])
+    betas = torch.linspace(-6, 6, timesteps)
+    betas = torch.sigmoid(betas)/(betas.max()-betas.min())*(0.02-betas.min())/10
+    return betas
+
+def cosine_noise_schedule(timesteps, s=0.008):
+    """
+    cosine schedule
+    as proposed in https://arxiv.org/abs/2102.09672
+    """
+    steps = timesteps + 1
+    x = torch.linspace(0, timesteps, steps, dtype=torch.float64)
+    alphas_cumprod = torch.cos(((x / timesteps) + s) / (1 + s) * math.pi * 0.5) ** 2
+    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
+    betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
+    return torch.clip(betas, 0, 0.999)
 
 class Diffusion:
     def __init__(
